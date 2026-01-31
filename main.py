9bee589f6ffa959ca0a942780771b77612f2e188
yutsz1203 <mervinyu3124@gmail.com>
2026-01-31 14:29:41 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,92 @@
+import re
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def main():
+    parse_home_page()
+
+def parse_race_card():
+    date = "2026/02/01"
+    racecourse = "ST"
+    race_no = "8"
+    url = f"https://racing.hkjc.com/zh-hk/local/information/racecard?racedate={date}&Racecourse={racecourse}&RaceNo={race_no}"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            content = response.text
+            soup = BeautifulSoup(content, "lxml")
+            racecard = soup.find(class_="starter f_tac f_fs13 draggable hiddenable")
+            pattern = r'horse'
+            home_pages = []
+            for anchor in racecard.find_all("a"):
+                home_page = anchor.get("href")
+                if re.search(pattern, home_page):
+                    home_pages.append(home_page)
+            data = []
+            header = racecard.find("thead")
+            if header:
+                cols = header.find("tr").find_all("td")
+                header_cols = [ele.get_text().strip() for ele in cols]
+
+            table = racecard.find("tbody")
+            if table:
+                rows = table.find_all("tr")
+                for row in rows:
+                    cols = [ele.get_text().strip() for ele in row.find_all(["td", "th"])]
+                    
+                    if cols:
+                        data.append(cols)
+
+                df = pd.DataFrame(data, columns=header_cols)
+                df.drop(columns=['綵衣', '烙號', '可能超磅', '國際評分','分齡讓磅', '性別', '今季獎金', '優先參賽次序','馬主', '父系', '母系', '進口類別'],inplace=True)
+                print(df)
+                df.to_excel(f"data/{date.replace("/", "")}_{racecourse}_{race_no}.xlsx", index=False)
+        else:
+            print(f"Failed to retrieve the contents.")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+def parse_home_page():
+    url = "https://racing.hkjc.com/zh-hk/local/information/horse?horseid=HK_2024_K434"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            content = response.text
+            soup = BeautifulSoup(content, "lxml")
+            results = soup.find(class_="bigborder")
+            if results:
+                rows = results.find_all("tr")
+                data = []
+                for row in rows:
+                    cols = [ele.get_text().strip() for ele in row.find_all(["td"])]
+                    if len(cols) > 1:
+                        data.append(cols)
+                header_col = data[0]
+                data = data[1:]
+                df = pd.DataFrame(data, columns=header_col)
+                df.drop(columns=['排位體重', '賽事重播'],inplace=True)
+                # df.to_excel("data/home_page.xlsx", index=False)
+                
+                indv_matches = []
+                pattern = r'localresults'
+                for anchor in results.find_all("a"):
+                    indv_match = anchor.get("href")
+                    if re.search(pattern, indv_match):
+                        indv_matches.append(indv_match) 
+                print(indv_matches)
+                                                                          
+        else:
+            print(f"Failed to retrieve the contents.")
+        
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
