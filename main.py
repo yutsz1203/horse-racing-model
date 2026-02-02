9b2eaebe554d6be290933f811902338a9da65a92
yutsz1203 <mervinyu3124@gmail.com>
2026-02-02 23:44:35 +0800

    chore: 2026/02/04 happy valley updates

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ idx_map = {
     "2400": 5,
 }
 
+track_translate = {"草地": "grass", "全天候": "dirt"}
+
 
 def main():
     """
@@ -32,132 +34,167 @@ def main():
     5. Combine all information and build df for each horse with their race history
     6. Build final df that shows last x match of each of the horses with the same distance and racecourse
     """
+    # "2026/02/04"
+    date = input("Input race date in this format (yyyy/mm/dd): ")
+    formatted_date = "".join(date.split("/"))
+    racecourse = input("Input racecourse (ST/HV): ")
+    total_race = input("Input total number of matches: ")
 
-    date = "2026/02/04"
-    racecourse = "HV"
-    raceno = "4"
-    home_pages, racecard_df = parse_race_card(date, racecourse, raceno)
-    # print(racecard_df)
-    # racecard_df fields = 馬匹編號           6次近績    馬名   負磅        騎師  檔位   練馬師  評分 評分+/-  排位體重 排位體重+/-     最佳時間 馬齡 上賽距今日數     配備
+    for rn in range(6, int(total_race) + 1):
+        raceno = str(rn)
+        home_pages, racecard_df, track, dist = parse_race_card(date, racecourse, raceno)
 
-    Path(f"data/{''.join(date.split("/"))}").mkdir(parents=True, exist_ok=True)
-    Path(f"data/{''.join(date.split("/"))}/{raceno}").mkdir(exist_ok=True)
-    for home_page, horse, horseno in zip(
-        home_pages, racecard_df["馬名"].values, racecard_df["馬匹編號"].values
-    ):
-        print(f"Processing {horse} match history...")
-        indv_matches, indv_df = parse_home_page(home_page)
-        df = indv_df.copy()
-        df.drop(
-            columns=["評分", "練馬師", "頭馬距離", "獨贏賠率", "配備"], inplace=True
+        # racecard_df fields = 馬匹編號           6次近績    馬名   負磅        騎師  檔位   練馬師  評分 評分+/-  排位體重 排位體重+/-     最佳時間 馬齡 上賽距今日數     配備
+
+        Path(f"data/{formatted_date}").mkdir(parents=True, exist_ok=True)
+        Path(f"data/{formatted_date}/{raceno}").mkdir(exist_ok=True)
+        Path(f"data/{formatted_date}/final").mkdir(exist_ok=True)
+
+        for home_page, horse, horseno in zip(
+            home_pages, racecard_df["馬名"].values, racecard_df["馬匹編號"].values
+        ):
+            print(f"Processing {horse} match history...")
+            indv_matches, indv_df = parse_home_page(home_page)
+            if indv_matches:
+                df = indv_df.copy()
+                df.drop(
+                    columns=["評分", "練馬師", "頭馬距離", "獨贏賠率", "配備"],
+                    inplace=True,
+                )
+
+                for indv_match in indv_matches:
+                    parsed_url = urlparse(indv_match)
+                    params = parse_qs(parsed_url.query)
+
+                    racedate = params.get("racedate", [None])[0]
+                    past_raceno = params.get("RaceNo", [None])[0]
+
+                    yr, mth, day = racedate.split("/")
+                    racedate_formatted = f"{day}/{mth}/{yr}"
+
+                    print(f"Processing the match on {racedate}")
+
+                    distance, pace, top_3, last_section_time = parse_sectional_time(
+                        horse, racedate_formatted, past_raceno
+                    )
+                    racedate_idx = pd.to_datetime(racedate_formatted, format="%d/%m/%Y")
+                    df.loc[racedate_idx, "日期"] = racedate_formatted
+                    df.loc[racedate_idx, "該仗步速"] = pace
+                    df.loc[racedate_idx, "第一名"] = top_3[0]
+                    df.loc[racedate_idx, "第二名"] = top_3[1]
+                    df.loc[racedate_idx, "第三名"] = top_3[2]
+                    df.loc[racedate_idx, "該仗末段"] = last_section_time
+
+                df["馬名"] = horse
+                df["馬匹編號"] = horseno
+                df["今仗檔位"] = racecard_df.loc[
+                    racecard_df["馬名"] == horse, "檔位"
+                ].iloc[0]
+                df["今仗騎師"] = racecard_df.loc[
+                    racecard_df["馬名"] == horse, "騎師"
+                ].iloc[0]
+                df["今仗負磅"] = racecard_df.loc[
+                    racecard_df["馬名"] == horse, "負磅"
+                ].iloc[0]
+
+                df.loc[df["馬場/跑道/賽道"].str.contains("跑馬地"), "馬場"] = "HV"
+                df.loc[df["馬場/跑道/賽道"].str.contains("沙田"), "馬場"] = "ST"
+                df.loc[df["馬場/跑道/賽道"].str.contains("草地"), "跑道"] = "grass"
+                df.loc[df["馬場/跑道/賽道"].str.contains("全天候"), "跑道"] = "dirt"
+
+                df.loc[df["賽事班次"].str.contains("G"), "賽事班次"] = "0"
+
+                standard_time_df = pd.read_csv("data/standard_time.csv")
+                standard_time_df["total_time"] = standard_time_df["total_time"].apply(
+                    parse_time
+                )
+                standard_time_df = standard_time_df.astype(
+                    {"class": "str", "distance": "str"}
+                )
+                df = df.merge(
+                    standard_time_df,
+                    left_on=["賽事班次", "馬場", "途程", "跑道"],
+                    right_on=["class", "race_course", "distance", "type"],
+                    how="left",
+                )
+                df["last_section_idx"] = df["途程"].map(idx_map)
+                df["完成時間"] = df["完成時間"].apply(parse_time)
+                df["該仗步速"] = df["該仗步速"].apply(parse_time)
+
+                col_indices = (df["last_section_idx"] + 26).astype(int).values
+                row_indices = np.arange(len(df))
+                df["該仗頭段"] = round(df["完成時間"] - df["該仗末段"], 2)
+                df["比標準頭段"] = round(
+                    df["該仗頭段"]
+                    - (df["total_time"] - df.values[row_indices, col_indices]),
+                    2,
+                )
+                df["比標準末段"] = round(
+                    df["該仗末段"] - df.values[row_indices, col_indices], 2
+                )
+                df["比標準時間"] = round(df["完成時間"] - df["total_time"], 2)
+
+                cols_to_drop = list(standard_time_df.columns) + [
+                    "last_section_idx",
+                ]
+                df.drop(
+                    columns=cols_to_drop,
+                    inplace=True,
+                )
+                df = df.rename(
+                    columns={
+                        "檔位": "上仗檔位",
+                        "騎師": "上仗騎師",
+                        "實際負磅": "上仗負磅",
+                    }
+                )
+                df = df[
+                    [
+                        "馬場",
+                        "跑道",
+                        "場地狀況",
+                        "途程",
+                        "場次",
+                        "日期",
+                        "名次",
+                        "馬名",
+                        "馬匹編號",
+                        "今仗檔位",
+                        "上仗檔位",
+                        "該仗步速",
+                        "該仗頭段",
+                        "比標準頭段",
+                        "該仗末段",
+                        "比標準末段",
+                        "完成時間",
+                        "比標準時間",
+                        "沿途走位",
+                        "第一名",
+                        "第二名",
+                        "第三名",
+                        "上仗騎師",
+                        "今仗騎師",
+                        "賽事班次",
+                        "上仗負磅",
+                        "今仗負磅",
+                    ]
+                ]
+                df.dropna(inplace=True)
+                df.to_csv(
+                    f"data/{''.join(date.split("/"))}/{raceno}/{horse}.csv", index=False
+                )
+
+        final_df, avg_df = concat_df(
+            Path(f"data/{formatted_date}/{raceno}"), racecourse, dist, track, 3
         )
-
-        for indv_match in indv_matches:
-            parsed_url = urlparse(indv_match)
-            params = parse_qs(parsed_url.query)
-
-            racedate = params.get("racedate", [None])[0]
-            past_raceno = params.get("RaceNo", [None])[0]
-
-            yr, mth, day = racedate.split("/")
-            racedate_formatted = f"{day}/{mth}/{yr}"
-
-            print(f"Processing the match on {racedate}")
-
-            distance, pace, top_3, last_section_time = parse_sectional_time(
-                horse, racedate_formatted, past_raceno
-            )
-            racedate_idx = pd.to_datetime(racedate_formatted, format="%d/%m/%Y")
-            df.loc[racedate_idx, "日期"] = racedate_formatted
-            df.loc[racedate_idx, "該仗步速"] = pace
-            df.loc[racedate_idx, "第一名"] = top_3[0]
-            df.loc[racedate_idx, "第二名"] = top_3[1]
-            df.loc[racedate_idx, "第三名"] = top_3[2]
-            df.loc[racedate_idx, "該仗末段"] = last_section_time
-
-        df["馬名"] = horse
-        df["馬匹編號"] = horseno
-        df["今仗檔位"] = racecard_df.loc[racecard_df["馬名"] == horse, "檔位"].iloc[0]
-        df["今仗騎師"] = racecard_df.loc[racecard_df["馬名"] == horse, "騎師"].iloc[0]
-        df["今仗負磅"] = racecard_df.loc[racecard_df["馬名"] == horse, "負磅"].iloc[0]
-
-        df.loc[df["馬場/跑道/賽道"].str.contains("跑馬地"), "馬場"] = "HV"
-        df.loc[df["馬場/跑道/賽道"].str.contains("沙田"), "馬場"] = "ST"
-        df.loc[df["馬場/跑道/賽道"].str.contains("草地"), "跑道"] = "grass"
-        df.loc[df["馬場/跑道/賽道"].str.contains("全天候"), "跑道"] = "dirt"
-
-        df.loc[df["賽事班次"].str.contains("G"), "賽事班次"] = "0"
-
-        standard_time_df = pd.read_csv("data/standard_time.csv")
-        standard_time_df["total_time"] = standard_time_df["total_time"].apply(
-            parse_time
+        final_df.to_csv(
+            f"{Path(f"data/{formatted_date}/final")}/{raceno}_final.csv", index=False
         )
-        standard_time_df = standard_time_df.astype({"class": "str", "distance": "str"})
-        df = df.merge(
-            standard_time_df,
-            left_on=["賽事班次", "馬場", "途程", "跑道"],
-            right_on=["class", "race_course", "distance", "type"],
-            how="left",
+        avg_df.to_csv(
+            f"{Path(f"data/{formatted_date}/final")}/{raceno}_avg.csv", index=False
         )
-        df["last_section_idx"] = df["途程"].map(idx_map)
-        df["完成時間"] = df["完成時間"].apply(parse_time)
-        df["該仗步速"] = df["該仗步速"].apply(parse_time)
-
-        col_indices = (df["last_section_idx"] + 26).astype(int).values
-        row_indices = np.arange(len(df))
-        df["該仗頭段"] = round(df["完成時間"] - df["該仗末段"], 2)
-        df["比標準頭段"] = round(
-            df["該仗頭段"] - (df["total_time"] - df.values[row_indices, col_indices]), 2
-        )
-        df["比標準末段"] = round(
-            df["該仗末段"] - df.values[row_indices, col_indices], 2
-        )
-        df["比標準時間"] = round(df["完成時間"] - df["total_time"], 2)
-
-        cols_to_drop = list(standard_time_df.columns) + [
-            "last_section_idx",
-            "馬場",
-            "跑道",
-        ]
-        df.drop(
-            columns=cols_to_drop,
-            inplace=True,
-        )
-        df = df.rename(
-            columns={"檔位": "上仗檔位", "騎師": "上仗騎師", "實際負磅": "上仗負磅"}
-        )
-        df = df[
-            [
-                "馬場/跑道/賽道",
-                "場地狀況",
-                "途程",
-                "場次",
-                "日期",
-                "名次",
-                "馬名",
-                "馬匹編號",
-                "今仗檔位",
-                "上仗檔位",
-                "該仗步速",
-                "該仗頭段",
-                "比標準頭段",
-                "該仗末段",
-                "比標準末段",
-                "完成時間",
-                "比標準時間",
-                "沿途走位",
-                "第一名",
-                "第二名",
-                "第三名",
-                "上仗騎師",
-                "今仗騎師",
-                "賽事班次",
-                "上仗負磅",
-                "今仗負磅",
-            ]
-        ]
-        df.dropna(inplace=True)
-        df.to_csv(f"data/{''.join(date.split("/"))}/{raceno}/{horse}.csv", index=False)
+        # print(final_df.head())
+        # print(avg_df.head())
 
 
 def parse_race_card(date, racecourse, raceno):
@@ -169,6 +206,12 @@ def parse_race_card(date, racecourse, raceno):
         if response.status_code == 200:
             content = response.text
             soup = BeautifulSoup(content, "lxml")
+
+            info_box = soup.find(class_="f_fs13")
+            info = info_box.text.split(",")
+            track_info = track_translate[info[3][-2:]]
+            dist_info = int(info[5][1:5])
+
             racecard = soup.find(class_="starter f_tac f_fs13 draggable hiddenable")
             pattern = r"horse"
             home_pages = []
@@ -217,7 +260,7 @@ def parse_race_card(date, racecourse, raceno):
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
-    return home_pages, df
+    return home_pages, df, track_info, dist_info
 
 
 def parse_home_page(home_page):
@@ -300,12 +343,15 @@ def parse_sectional_time(target, racedate, raceno):
                         top_3.append(horse_name)
                     if horse_name == target:
                         # extract last sectional time
-                        last_section = row.find_all("td")[idx + 3]
-                        last_section_time = float(
-                            last_section.find(class_="sectional_200").text.split(
-                                "\n", 1
-                            )[0]
+                        last_section = row.find_all("td")[idx + 3].find(
+                            class_="sectional_200"
                         )
+                        if last_section:
+                            last_section_time = last_section.text.split("\n", 1)[0]
+                            if last_section_time != "":
+                                last_section_time = float(last_section_time)
+                            else:
+                                last_section_time = None
 
         else:
             print("Failed to retrieve the contents.")
@@ -344,6 +390,42 @@ def create_standard_time_data():
     pass
     # df = pd.read_excel("data/standard_time.xlsx")
     # df.to_csv("data/standard_time.csv", index=False)
+
+
+def concat_df(dir: Path, racecourse, dist, track, recent_x):
+    all_dfs = []
+    avg = []
+
+    for file_path in dir.glob("*.csv"):
+        tmp_df = pd.read_csv(file_path)
+        cond = (
+            (tmp_df["馬場"] == racecourse)
+            & (tmp_df["跑道"] == track)
+            & (tmp_df["途程"] == dist)
+        )
+        filtered_df = tmp_df.loc[cond]
+
+        if not filtered_df.empty:
+            filtered_df = filtered_df.iloc[: min(recent_x, len(filtered_df))]
+            avg.append(
+                {
+                    "馬名": filtered_df["馬名"].values[0],
+                    "完成時間": filtered_df["完成時間"].mean().round(2),
+                }
+            )
+            all_dfs.append(filtered_df)
+
+    if all_dfs:
+        final_df = pd.concat(all_dfs, ignore_index=True)
+        final_df.sort_values(by="比標準時間", inplace=True)
+
+        avg_df = pd.DataFrame(avg)
+        avg_df.sort_values(by="完成時間", inplace=True)
+    else:
+        final_df = pd.DataFrame()
+        avg_df = pd.DataFrame()
+
+    return final_df, avg_df
 
 
 if __name__ == "__main__":
